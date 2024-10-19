72ce8e0189efc2990a40b9fef4f8227c422c2387
Deilham Fachrezi <112366873+deilhamf@users.noreply.github.com>
2024-10-19 23:01:43 +0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
 
                 <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
                     <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-                        I&apos;m usually called Deil. I like to learn new things even if it&apos;s just a little bit. I am interested in computer technology and foreign languages. If you&apos;re having trouble with your project, i&apos;m ready to help you finish it. i&apos;ll make you a good and aesthetic result. feel free to contact me!, i&apos;ll be a good friend to you. lol. :D
+                        Hi, I&apos;m usually called Deil. I like to learn new things even if it&apos;s just a little bit. I am interested in computer technology and foreign languages. If you&apos;re having trouble with your project, i&apos;m ready to help you finish it. i&apos;ll make you a good and aesthetic result. feel free to contact me!, i&apos;ll be a good friend to you. lol. :D
                     </p>
 
                     <div className="flex flex-wrap items-center gap-4 md:gap-7">
